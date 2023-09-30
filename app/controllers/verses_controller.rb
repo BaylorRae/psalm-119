a2df9db0465bedf997cdada1a8cdecede470170c
Baylor Weathers <baylor@thecodedeli.com>
2023-09-30 16:08:30 -0500

    group verses with alphabet

--- app/controllers/verses_controller.rb
+++ app/controllers/verses_controller.rb
@@ -1,10 +1,35 @@
 class VersesController < ApplicationController
   def index
-    @verses = Verse.order(:number)
+  @verses = Verse.order(:number).in_groups_of(8)
   end
 
   def show
     @verse = Verse.find_by(number: params[:number])
     @commentaries = @verse.commentaries
   end
+
+  LETTERS = [
+    "א ALEPH",
+    "ב BETH",
+    "ג GIMEL",
+    "ד DALETH",
+    "ה HE",
+    "ו VAU",
+    "ז ZAIN",
+    "ח CHETH",
+    "ט TETH",
+    "י JOD",
+    "כ CAPH",
+    "ל LAMED",
+    "מ MEM",
+    "נ NUN",
+    "ס SAMECH",
+    "ע AIN",
+    "פ PE",
+    "צ TZADDI",
+    "ק KOPH",
+    "ר RESH",
+    "ש SCHIN",
+    "ת TAU"
+  ]
 end
